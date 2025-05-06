591713c00beb62b11db747e53a0a1f012d52dba4
NNF42 <vinicios.abrantes17@gmail.com>
2025-05-06 17:30:05 -0300

    Teste de Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Avatar, Button, Card } from "@chakra-ui/react"
+import LandingPage from "./pages/LandingPage"
+
+
+function App() {
+
+  return (
+    <>
+      <LandingPage/>
+    </>
+  )
+}
+
+export default App
